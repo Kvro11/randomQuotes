bfa7e2d27f33d2f3d34445800196e1f27c206bba
Jester <jesterjayoma04@gmail.com>
2025-03-27 18:24:27 +0800

    optimized page

--- src/QuotesContainer/index.jsx
+++ src/QuotesContainer/index.jsx
@@ -75,8 +75,8 @@ const QuotesContainer = (props) => {
         </div>
         <button
           className={`py-2 px-4 relative ${bgColor} bg-opacity-50 rounded-lg 
-    transition-transform transition-opacity ease-in-out 
-    duration-200 hover:scale-90 hover:opacity-80 font-semibold`}
+            transition ease-in-out duration-200 transform opacity-100 
+            shover:scale-90 hover:opacity-80 font-semibold`}
           onClick={handleNewQuotes}
         >
           New Quotes
